fab42d3dc3096f1292cc5aa0ed1f629c252e1ac8
Rob Streeting <robsws@gmail.com>
2023-04-06 14:36:56 +0100

    fix: fit window to border in browser

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,16 @@ mod inner;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Inner Demons".into(),
+                resolution: (1000.0, 800.0).into(),
+                fit_canvas_to_parent: true,
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         // .add_startup_system(debug_setup)
         // .add_system(spawn_player)
         .add_plugin(CardGamePlugin)
